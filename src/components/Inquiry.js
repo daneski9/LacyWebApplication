c96d46306b6d8d67977e8ae72bd876a655a3ac09
ioleshko1 <117392319+ioleshko1@users.noreply.github.com>
2023-04-02 10:18:11 -0700

    Change to base file

--- src/components/Inquiry.js
+++ src/components/Inquiry.js
@@ -1,37 +1,15 @@
 import React, { useState } from 'react';
 
 function InquiryPage() {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${name}`);
-    console.log(`Email: ${email}`);
-    console.log(`Message: ${message}`);
-    // Do something with the form data, e.g. send it to a server
-  };
-
   return (
     <div>
-      <h1>Inquiry Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" value={message} onChange={(event) => setMessage(event.target.value)} required />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <h1 className="title is-1">This is the About Page</h1>
+      <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
+          risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
+          nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
+          lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
+      </p>
     </div>
   );
 }
