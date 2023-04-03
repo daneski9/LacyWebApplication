9b7772aae7d36fa51f87351172e4e4b7ee26248c
Igor Oleshko <ioleshko@csus.edu>
2023-04-03 11:33:31 -0700

    # JIRA Task 38
    # This code modified the Inquiry page which will now have several fields that are visible to the user.

--- src/components/Inquiry.js
+++ src/components/Inquiry.js
@@ -1,15 +1,69 @@
 import React, { useState } from 'react';
 
 function InquiryPage() {
+
+  // Hold all of the states of the fields that we need to use for the email
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [location, setLocation] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null);
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // We will need to add additional stuff here to lead to other pages in capture data in Sprint 04
+    
+  };
+
   return (
     <div>
-      <h1 className="title is-1">This is the About Page</h1>
-      <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
-          risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
-          nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
-          lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
-      </p>
+
+      <h1>Inquiry Form</h1>
+      <form onSubmit={handleSubmit}>
+        
+        <div>
+          <label htmlFor="first">First Name:</label>
+          <input type="text" id="first" value={first} onChange={(event) => setFirst(event.target.value)} required />
+        </div>
+
+        <div>
+          <label htmlFor="last">Last Name:</label>
+          <input type="text" id="last" value={last} onChange={(event) => setLast(event.target.value)} required />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
+        </div>
+
+        <div>
+          <label htmlFor="number">Phone Number:</label>
+          <input type="tel" id="number" value={number} onChange={(event) => setNumber(event.target.value)} required />
+        </div>
+
+        <div>
+          <label htmlFor="location">Location on the body:</label>
+          <input type="text" id="location" value={location} onChange={(event) => setLocation(event.target.value)} required />
+        </div>
+
+        <div>
+          <label htmlFor="description">Tattoo Description:</label>
+          <textarea id="description" value={description} onChange={(event) => setDescription(event.target.value)} required />
+        </div>
+
+        <div>
+          <label htmlFor="image">Reference Image:</label>
+          <input type="file" id="image" onChange={(event) => setImage(event.target.files[0])} required />
+        </div>
+
+        <button type="submit">Submit</button>
+
+      </form>
+
     </div>
   );
 }
