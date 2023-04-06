4f779b2e28a61842b034e36ff2988b700ce9ae4d
https://ecs-pw-gitlab.ecs.csus.edu/ashackelford <ashackelford@csus.edu>
2023-04-05 23:57:42 -0700

    no errors 11:57pm

--- src/components/julioj/PortfolioJulioJimenez.js
+++ src/components/julioj/PortfolioJulioJimenez.js
@@ -7,11 +7,13 @@ import './PortfolioJulioJimenez.css';
 function Portfolio(properties) {
     const [query, setQuery] = useState('');
 
-    // The directory path where the images are located
-    const dirPath = '../images/';
-
     // An array to hold the image file names
-    const imageArray = [];
+    const [imageArray, setImageArray] = useState([]);
+
+    // The directory path where the images are located
+    const dirPath = './images/';
+
+    let x = 0;
 
     // Load the images from the server and add them to the array
     fetch(dirPath)
@@ -21,10 +23,13 @@ function Portfolio(properties) {
         const doc = parser.parseFromString(html, 'text/html');
         const imgElements = doc.querySelectorAll('img');
         imgElements.forEach(img => {
-        const src = img.getAttribute('src');
+            x += 1;
+            const src = img.getAttribute('src');
             imageArray.push(src);
         });
         console.log(imageArray);
+        console.log(x);
+        setImageArray(imageArray);
     })
     .catch(error => {
         console.error('Error loading images:', error);
@@ -34,6 +39,7 @@ function Portfolio(properties) {
     const dataArray = new Array(imageArray.length);
 
     imageArray.forEach(element => dataArray.push(element));
+    // FIXME console says this is empty...
 
     return (
         <>
@@ -49,10 +55,13 @@ function Portfolio(properties) {
             >
             <SearchBar query={query} onChange={setQuery} />
             </div>
+            <div>
+                <textarea rows={5} style={{ width: '100%' }} value={JSON.stringify(imageArray, null, 2)} readOnly />
+            </div>
             <div className="bannerGrid">
-            {dataArray.map((image) => (
-                <Box data={dirPath + image} key={image} />
-            ))}
+                {imageArray.map((image) => (
+                    <Box data={dirPath + image} key={image} />
+                ))}
             </div>
         </>
     );
