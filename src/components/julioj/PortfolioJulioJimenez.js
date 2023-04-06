9b5ac49012531ba336e7be53a09c90e8e3996688
https://ecs-pw-gitlab.ecs.csus.edu/ashackelford <ashackelford@csus.edu>
2023-04-05 19:38:00 -0700

    save point 1, restarting

--- src/components/julioj/PortfolioJulioJimenez.js
+++ src/components/julioj/PortfolioJulioJimenez.js
@@ -2,10 +2,35 @@ import { useState, useEffect } from "react";
 import Navbar from "../Navbar";
 import SearchBar from "../subcomponents/Searchbar";
 import Boxes from "../subcomponents/Boxes";
+import Box from "../subcomponents/Box";
 
 function Portfolio(properties) {
     const [query, setQuery] = useState('');
     const [images, setImages] = useState([]);
+
+    // The directory path where the images are located
+    const dirPath = '../images/';
+
+    // An array to hold the image file names
+    const imageArray = [];
+
+    
+    // Load the images from the server and add them to the array
+    fetch(dirPath)
+    .then(response => response.text())
+    .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const imgElements = doc.querySelectorAll('img');
+        imgElements.forEach(img => {
+            const src = img.getAttribute('src');
+            imageArray.push(src);
+        });
+        console.log(imageArray);
+    })
+    .catch(error => {
+        console.error('Error loading images:', error);
+    });
   
     useEffect(() => {
         // get all images from the images directory
@@ -26,9 +51,12 @@ function Portfolio(properties) {
                     query={query}
                     setQuery={setQuery}
                 />
-                <Boxes
-                    filteredImages={filteredImages}
-                />
+                
+            </div>
+            <div className="Boxes">
+                {imageArray.map((banner) => (
+                <Box data={banner}/>
+                ))}
             </div>
         </>
     );
