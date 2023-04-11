6aa4a02ca84e1bf6292eee094d828ab1e72d2e32
Dane <91101595+daneski9@users.noreply.github.com>
2023-04-11 14:34:10 -0700

    #79 Added CSS to about page (#34)

--- src/components/julioj/AboutJulioJimenez.js
+++ src/components/julioj/AboutJulioJimenez.js
@@ -8,25 +8,25 @@ function About() {
       <Navbar />
       
       <div class = "top-container">
-      <img src={julioPic} alt="julio-pic" />
-      <p>While tattooing since 2016, Julio specializes in realism and black ink. He studies realistic art and photo realism. In his spare time, Julio draws
-        and paints constantly, trying to develop skills and raise the bar!</p>
+        <img src={julioPic} alt="julio-pic" />
+        <div class = "text-about">
+          <p>While tattooing since 2016, Julio specializes in realism and black ink. He studies realistic art and photo realism. In his spare time, Julio draws
+            and paints constantly, trying to develop skills and raise the bar!</p>
+          </div>
       </div>
       <div class = "hours-container">
-        <h2>Hours</h2>
-        <ul>
-          <li>M 10am - 9pm</li>
-          <li>T  10am - 9pm</li>
-          <li>W 10am - 9pm</li>
-          <li>F  10am - 9pm</li>
-          <li>Sa (Appointment Only)</li>
-          <li>Su (Appointment Only)</li>
-        </ul>
+        <div class = "hours-info">
+          <h2>Hours</h2>
+          <ul>
+            <li>M&nbsp;&nbsp;10am - 9pm</li>
+            <li>T&nbsp;&nbsp;&nbsp;10am - 9pm</li>
+            <li>W 10am - 9pm</li>
+            <li>F&nbsp;&nbsp;&nbsp;10am - 9pm</li>
+            <li>Sa (Appointment Only)</li>
+            <li>Su (Appointment Only)</li>
+          </ul></div>
       </div>
 
-      <div class="bottom-container">
-        <a class = "socials" href = "https://instagram.com" target="_blank" rel ="noreferrer">Instagram</a>
-      </div>
       
       </>
     );
