34d35a1d109f4816901729937f487e50000adcb0
Dane <91101595+daneski9@users.noreply.github.com>
2023-04-12 12:29:21 -0700

    #124 Added inquire btn to bottom of about page. (#36)

--- src/components/julioj/AboutJulioJimenez.js
+++ src/components/julioj/AboutJulioJimenez.js
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
 import Navbar from "../Navbar";
-import julioPic from "../images/julio/julio-pic.png";
-import './julioCSS/AboutJulioJimenez.css'
+import julioPic from "../images/julio/j-tats.png";
+import './julioCSS/AboutJulioJimenez.css';
 
 function About() {
     return (
@@ -8,11 +9,12 @@ function About() {
       <Navbar />
       
       <div class = "top-container">
-        <img src={julioPic} alt="julio-pic" />
-        <div class = "text-about">
-          <p>While tattooing since 2016, Julio specializes in realism and black ink. He studies realistic art and photo realism. In his spare time, Julio draws
+        <img src={julioPic} alt="j-tats" />
+        <div class = "text-box">
+          <div class = "text-content"> <p>While tattooing since 2016, Julio specializes in realism and black ink. He studies realistic art and photo realism. In his spare time, Julio draws
             and paints constantly, trying to develop skills and raise the bar!</p>
           </div>
+        </div>
       </div>
       <div class = "hours-container">
         <div class = "hours-info">
@@ -25,6 +27,11 @@ function About() {
             <li>Sa (Appointment Only)</li>
             <li>Su (Appointment Only)</li>
           </ul></div>
+      </div>
+      <div class = "bottom-container">
+        <Link to="/JulioJimenez/inquiry">
+        <button class ='about-btn'>BOOK NOW</button>
+        </Link>
       </div>
 
       
