319f1c82216fa0716a20837d5066570f015022c2
Eric.Chavez <jimenezeric530@gmail.com>
2023-04-19 14:18:15 -0700

    #121 Added "Navigation" Title

--- src/components/julioj/Footer.js
+++ src/components/julioj/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
           <p>Email: Email to be added</p>
         </div>
         <div className="footer-middle">
-          <h3>Find Us</h3>
+          <h3>Naviagtion</h3>
           <ul>
           <li><a href= "/JulioJimenez/about"> About </a></li> 
           <li><a href= "/JulioJimenez/services"> Services </a></li>
