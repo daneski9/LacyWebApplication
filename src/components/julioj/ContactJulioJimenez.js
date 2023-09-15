3ab2a3eb4711e8c87a32a72eff94c8194d7a3b40
carlenacodes <60952500+carlenacodes@users.noreply.github.com>
2023-09-15 02:27:04 -0700

    #71 Send Email Via Contact Page (#63)

--- src/components/julioj/ContactJulioJimenez.js
+++ src/components/julioj/ContactJulioJimenez.js
@@ -1,11 +1,15 @@
 import Navbar from "../Navbar";
 import React, { useState } from 'react';
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 //import './julioCSS/juliojimenez.css';
 import Footer from './Footer';
 //import '../../App.css'
 import './julioCSS/ContactJulioJimenez.css'
 
 function Contact() {
+
+  const form = useRef()
     
   const [first, setFirst] = useState('');
   const [last, setLast] = useState('');
@@ -13,32 +17,26 @@ function Contact() {
   const [message, setDescription] = useState('');
   const [showAlert, setShowAlert] = useState(false);
 
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    emailjs.sendForm('service_26d3yqg', 'template_gm183as', form.current, 'xcT2ZwmVbDNdY3rvM')
+      .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
 
-     // Clear all fields
-     setFirst('');
-     setLast('');
-     setEmail('');
-     setDescription('');
-     
- 
-     // Show alert
-     setShowAlert(true);
- 
-     // Hide alert after 15 seconds
-     setTimeout(() => {
-       setShowAlert(false);
-     }, 15000);
+    e.target.reset();
 
-    
-    
+    // Show alert
+    setShowAlert(true);
+
+    // Hide alert after 15 seconds
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 15000);
   };
-  
-  
-  
-  
   
   return (
       <>
@@ -58,12 +56,12 @@ function Contact() {
             nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
             lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
           </p> */}
-          <form onSubmit={handleSubmit}>
+          <form ref={form} onSubmit={sendEmail}>
           
-            <input type="text" id="first" placeholder="First name" value={first} onChange={(event) => setFirst(event.target.value)} required />
-            <input type="text" id="last" placeholder="Last name" value={last} onChange={(event) => setLast(event.target.value)} required />
-            <input type="email" id="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} required />
-            <textarea id="message" placeholder="Message" value={message} onChange={(event) => setDescription(event.target.value)} required />
+            <input type="text" name="first" placeholder="First name" value={first} onChange={(event) => setFirst(event.target.value)} required />
+            <input type="text" name="last" placeholder="Last name" value={last} onChange={(event) => setLast(event.target.value)} required />
+            <input type="email" name="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} required />
+            <textarea id="message" name ="message" placeholder="Message" value={message} onChange={(event) => setDescription(event.target.value)} required />
 
             <div className='submit-button'>
               <button type="submit">Submit</button>
