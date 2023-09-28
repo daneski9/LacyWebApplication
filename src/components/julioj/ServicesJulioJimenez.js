c3e3d1292e8c1798a3c44a2687ac9f33681f7186
Dane Coleman <danecoleman@csus.edu>
2023-09-27 18:21:06 -0700

    update services page layout

--- src/components/julioj/ServicesJulioJimenez.js
+++ src/components/julioj/ServicesJulioJimenez.js
@@ -1,112 +1,129 @@
 import Navbar from "../Navbar";
-import ServiceBanner from "../subcomponents/ServiceBanner";
+//import ServiceBanner from "../subcomponents/ServiceBanner";
 import './julioCSS/ServicesJulioJimenez.css';
+import './julioCSS/InquiryJulioJimenez.css'
+import { Link } from 'react-router-dom';
 import Footer from './Footer';
-import servicePic1 from "../images/services/full_body.jpeg";
-import servicePic2 from "../images/services/half_day.webp";
-import servicePic3 from "../images/services/upper_arm.jpeg";
-import servicePic4 from "../images/services/lower_arm.jpeg";
-import servicePic5 from "../images/services/bicep_tattoo.jpeg";
-import servicePic6 from "../images/services/lower_leg.jpeg";
-import servicePic7 from "../images/services/chest.jpeg";
-import servicePic8 from "../images/services/spine.webp";
+import servicePic1 from "../images/services/img1.jpg";
+import servicePic2 from "../images/services/img2.jpg";
+import servicePic3 from "../images/services/img3.jpg";
+import servicePic4 from "../images/services/img4.jpg";
+import servicePic5 from "../images/services/img5.jpg";
+import servicePic6 from "../images/services/img6.jpg";
+import servicePic7 from "../images/services/img7.jpg";
+import servicePic8 from "../images/services/img8.png";
 
-const leftArray = [
-  {
-    image: servicePic1,
-    session: 'Full Day Session',
-    price: 1600,
-    deposit: 200,
-    time: 480,
-    description: 'Book this session if interested in starting a full back, chest or sleeve'
-  },
-  {
-  image: servicePic3,
-  session: 'Upper Arm Session',
-  price: 2400,
-  deposit: 200,
-  time: 480,
-  description: 'Upper Arm Tattoo may exceed 8hrs or require 2 Sessions'
-  },
-  {
-    image: servicePic5,
-    session: 'Inner Bicep Session',
-    price: 1200,
-    deposit: 200,
-    time: 420,
-    description: 'Full Inner biceps tattoo between the elbow and the armpit'
-  }, 
-  {
-    image: servicePic7,
-    session: 'Chest Tattoo',
-    price: 1400,
-    deposit: 200,
-    time: 480,
-    description: 'Single side chest tattoo that covers whole chest from. Takes Full day'
-  }
-]
-
-
-const rightArray = [
-{
-  image: servicePic2,
-  session: 'Half day Session',
-  price: 1200,
-  deposit: 200,
-  time: 480,
-  description: 'Typically Involves a 3-4 hour appointment with a professional tattoo artist'
-},
-{
-  image: servicePic4,
-  session: 'Full Lower Arm',
-  price: 1200,
-  deposit: 200,
-  time: 420,
-  description: 'Full Inner Forearm Tattoo Wrist to Ditch Or Full Outer Arm Tattoo Wrist to Elbow'
-},
-{
-  image: servicePic6,
-  session: 'Full Lower Leg',
-  price: 1600,
-  deposit: 200,
-  time: 480,
-  description: 'Full Lower Leg Tatttoo inner ankle to knee or outer ankle to knee'
-},
-{
-  image: servicePic8,
-  session: 'Spine Tattoo',
-  price: 700,
-  deposit: 200,
-  time: 360,
-  description: 'Book this session if interested in starting a full back, chest or sleeve'
-}
-]
 
 function Services() {
   return (
     <>
       <Navbar />
-
-      <h1 class="services_header">Services Provided</h1>
-
-      <ul class = "servicesList">
-        <li>
-          {leftArray.map((banner) => (
-            <div className="leftSide">
-              <ServiceBanner data={banner}/>
-            </div>
-          ))}
-        </li>
-        <li>
-          {rightArray.map((banner) => (
-            <div className="rightSide">
-              <ServiceBanner data={banner}/>
-            </div>
-          ))}
-        </li>
-      </ul>
-
-      <Footer />
+    
+    <h1 className="servicesTitle">Services</h1>
+    <div className="page-container">
+       <div className = "services-container">
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic1} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic2} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic3} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic4} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic5}  alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic6}alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic7} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic8} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic7} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic7} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+              <Link to="/JulioJimenez/inquiry">
+              <div className = "services-item">
+                  <img className = "itemImg" src={servicePic7} alt="text"></img>
+                  <div className = "itemText">
+                    <p class = "itemTitle">Full day session</p>
+                    <p>Book this session if interested in starting a full back, chest or sleeve</p>
+                  </div>
+              </div>
+              </Link>
+        </div>
+    </div>
+      <Footer className="footer"/>
     </>
   );
 }
