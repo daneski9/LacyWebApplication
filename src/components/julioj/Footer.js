41cfa02f263f59eb914bd8a10c7f919fa078cc23
Ziwei Fang <fzw8833@yahoo.com>
2023-10-01 21:49:57 -0700

    176-add instagram icon

--- src/components/julioj/Footer.js
+++ src/components/julioj/Footer.js
@@ -33,6 +33,11 @@ function Footer(props) {
           <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.458103093944!2d-118.22081752365085!3d34.083403116083176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75bb49ff2b7%3A0x198631892d3409d2!2s2684%20Lacy%20St%2C%20Los%20Angeles%2C%20CA%2090031!5e0!3m2!1sen!2sus!4v1694672824199!5m2!1sen!2sus" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
+        <div className = "footer-middle2"> 
+        <h3>Julio's Instagram Page</h3> 
+        <a href="https://www.instagram.com/jayytatts/?hl=en" target="_blank"> 
+        <img src= {require("../images/insIcon.png")} alt="julio" /> </a> 
+        </div>
       </div>
       <div className="footer-bottom">
         <p>Copyright © 2023 Lacy St. Art Lounge.
