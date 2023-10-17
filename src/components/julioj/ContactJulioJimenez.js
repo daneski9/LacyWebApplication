2e4bb01c080352ad104c630224dbd83604cea24a
ioleshko1 <117392319+ioleshko1@users.noreply.github.com>
2023-10-16 17:50:13 -0700

    #253, #254: Image Submission and Contact Page (#103)
    * #254: Only Allow Submission When Image is Attached
    * #253: Update Contact Page w Customer Requirements

--- src/components/julioj/ContactJulioJimenez.js
+++ src/components/julioj/ContactJulioJimenez.js
@@ -1,7 +1,9 @@
 import Navbar from "../Navbar";
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {useRef} from 'react';
 import emailjs from '@emailjs/browser';
+
 //import './julioCSS/juliojimenez.css';
 import Footer from './Footer';
 //import '../../App.css'
@@ -58,8 +60,10 @@ function Contact() {
       )}
         
       <h1 className="contact_header">Contact</h1>
+      
       <form ref={form} onSubmit={sendEmail}>
-            
+        <p className="messageText">Have any questions or concerns? Send me an email here.</p>
+
         <input type="text" name="first" placeholder="First name" value={first} onChange={(event) => setFirst(event.target.value)} required />
         <input type="text" name="last" placeholder="Last name" value={last} onChange={(event) => setLast(event.target.value)} required />
         <input type="email" name="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} required />
@@ -70,6 +74,18 @@ function Contact() {
         
               
       </form>
+
+      <br /><br /><br /><br /><br /><br /><br />
+      <h1>Looking to Schedule an Appointment?</h1>
+      <Link to="/JulioJimenez/inquiry">
+            <button className='about-btn' onClick={() => {
+              window.scroll({
+                top: 0,
+                left: 0
+              });
+            }}>BOOK NOW</button>
+          </Link>
+
     </div>
 
     <Footer />
@@ -78,7 +94,4 @@ function Contact() {
   );
 }
   
-  export default Contact;
-
-
-  
+export default Contact;
