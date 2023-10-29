28e43c926f2974941703bdea8cf4b2a80bc793b0
carlenacodes <carlenakitley@gmail.com>
2023-10-29 14:46:23 -0700

    LWA #267, 268 Dashboard refresh fix add to nav bar

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { Route, Navigate, Outlet } from 'react-router-dom';
-import { getAuth } from 'firebase/auth';
+import React, { useState, useEffect } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function PrivateRoute({ path, ...props }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
   const auth = getAuth();
-  const isAuthenticated = auth.currentUser !== null; // Check if the user is authenticated
 
-  return isAuthenticated ? 
-    <Outlet /> : <Navigate to="/JulioJimenez/login" />;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe(); 
+  }, [auth]);
+
+  if (isAuthenticated === null) {
+    return null; 
+  }
+  return isAuthenticated ? <Outlet /> : <Navigate to="/JulioJimenez/login" />;
 }
-
-
-
-/*
-const PrivateRoute = () => {
-    const auth = null; // determine if authorized, from context or however you're doing it
-
-    // If authorized, return an outlet that will render child elements
-    // If not, return element that will navigate to login page
-    return auth ? <Outlet /> : <Navigate to="/JulioJimenez/login" />;
-}
-
-*/
 
 export default PrivateRoute;
