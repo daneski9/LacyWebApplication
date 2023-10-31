12761c043f0e6a9a62f4548fe533449002218d4c
Dane <91101595+daneski9@users.noreply.github.com>
2023-10-30 20:02:16 -0700

    sort-by-latest pics in portfolio. small css changes to dash. (#127)

--- src/components/julioj/PortfolioJulioJimenez.js
+++ src/components/julioj/PortfolioJulioJimenez.js
@@ -3,31 +3,56 @@ import Navbar from "../Navbar";
 import './julioCSS/PortfolioJulioJimenez.css';
 import Footer from './Footer';
 import { storage } from "../../DataBase";
-import { ref, listAll, getDownloadURL } from "firebase/storage";
+import { ref, listAll, getDownloadURL, getMetadata } from "firebase/storage";
 import { Link } from 'react-router-dom';
 
 
 
 function Portfolio(properties) {
 
-    // A List to hold the image file names
-    // reference video: https://www.youtube.com/watch?v=YOAeBSCkArA
     const [imageList, setImageList] = useState([]);
     const imageListRef = ref(storage, "Portfolio-page/");
 
     useEffect(() => {
-        listAll(imageListRef)
-            .then((response) => {
-                const downloadPromises = response.items.map((item) => {
-                    return getDownloadURL(item);
+        // Define an async function inside the useEffect
+        const fetchAndSortImages = async () => {
+            try {
+                // Fetch all items from the provided Firebase storage reference.
+                const response = await listAll(imageListRef);
+                
+                // For each item in the response, get its metadata.
+                const itemsWithMetadata = await Promise.all(
+                    response.items.map(async item => {
+                        const metadata = await getMetadata(item);
+                        return { item, metadata };
+                    })
+                );
+    
+                // Sort items based on their updated date in descending order.
+                const sortedItemsWithMetadata = itemsWithMetadata.sort((a, b) => {
+                    const aDate = new Date(a.metadata.updated);
+                    const bDate = new Date(b.metadata.updated);
+                    return bDate - aDate;
                 });
     
-                Promise.all(downloadPromises)
-                    .then((urls) => {
-                        setImageList(urls);
-                    });
-            });
-    }, [imageListRef]);
+                // For each sorted item, fetch its download URL.
+                const sortedUrls = await Promise.all(
+                    sortedItemsWithMetadata.map(({ item }) => getDownloadURL(item))
+                );
+    
+                // Once all URLs are fetched, update the state with the sorted URLs.
+                setImageList(sortedUrls);
+            } catch (error) {
+                // Handle any errors that might occur during the async operations
+                console.error("Error fetching and sorting images:", error);
+            }
+        };
+    
+        // Invoke the async function
+        fetchAndSortImages();
+        
+    }, [imageListRef]);  // The effect hook will re-run when the imageListRef changes.
+    
     
     const [selectedImage, setSelectedImage] = useState(null);
     const [selectedImageIndex, setSelectedImageIndex] = useState(null);
