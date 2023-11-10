38001d7dc03dba387f6654c6082ae3f310c8e20d
Abdul Malek <malekus40@gmail.com>
2023-11-10 10:58:05 -0800

    #238 fix google map api errors

--- src/components/julioj/Footer.js
+++ src/components/julioj/Footer.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import "./julioCSS/Footer.css";
+import { useJsApiLoader } from '@react-google-maps/api';
+import Map from "../Map";
 
 function Footer(props) {
 
   const handleLinkClick = () => {
     window.scrollTo(0, 0);
   };
+  // const { isLoaded } = useJsApiLoader ({
+  //   id: "AIzaSyBwZIHTwjOEb62DXwjnuKziwBwWvK7AM3U",
+  //   googleMapsApiKey: "AIzaSyBwZIHTwjOEb62DXwjnuKziwBwWvK7AM3U",
+  // });
 
   return (
+    
+    
+
     <footer className={props.className}>
       <div className="footer-container">
         <div className="footer-left">
@@ -15,7 +24,7 @@ function Footer(props) {
           <p>2684 Lacy St.</p>
           <p>Los Angeles, CA 90031</p>
           <p>Phone: (562) 201-7139</p>
-          <p>Email: lacystartlounge<br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@gmail.com</p>
+          <p>Email: Email to be added</p>
         </div>
         <div className="footer-middle">
           <h3>Navigation</h3>
@@ -29,9 +38,7 @@ function Footer(props) {
         </div>
         <div className="footer-right">
           <h3>Where We Are</h3>
-          <div>
-          <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.458103093944!2d-118.22081752365085!3d34.083403116083176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75bb49ff2b7%3A0x198631892d3409d2!2s2684%20Lacy%20St%2C%20Los%20Angeles%2C%20CA%2090031!5e0!3m2!1sen!2sus!4v1694672824199!5m2!1sen!2sus" referrerPolicy="no-referrer-when-downgrade"></iframe>
-          </div>
+          <Map/>
         </div>
       </div>
       <div className="footer-bottom">
